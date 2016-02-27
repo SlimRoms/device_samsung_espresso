Unlegacy-Android/android_device_samsung_espresso

Set up unified build for wifi-only variants

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo aosp_espresso-userdebug

SlimRoms/device_samsung_espresso

Change-Id: I9fffe523aefac96e2cd0461b9398806b02e4c131

vendorsetup.sh
@@ -1,0 +1,1 @@
+add_lunch_combo slim_espresso-userdebug